b99f6553c3f6711fb88719eb487ec25b9b6105f2
JSwys <jswysgood@gmail.com>
2022-03-09 10:27:10 -0500

    navbar 1.1

--- js/DonutMaker-App.js
+++ js/DonutMaker-App.js
@@ -57,9 +57,22 @@ multiButton.addEventListener("click", () => {
 
 //dropdown nav
 const dropdownBtnEl = document.querySelector(".dropbtn");
+const windowEl = document.getElementById
 
 
 dropdownBtnEl.addEventListener("click", () => {
   document.getElementById("myDropdown").classList.toggle("show");
 })
 
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
